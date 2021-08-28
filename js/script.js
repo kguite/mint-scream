718d64fb4e5a450aa3f515e0afa43d05aaa6b83d
KatieMarie <kguite@gmail.com>
2021-08-27 20:45:30 -0700

    parallax background changes, add backups

---
+++ js/script.js
@@ -0,0 +1,85 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let score = 0;
+ctx.font = '50px Impact';
+
+let timeToNextRaven = 0;
+let ravenInterval = 500;
+let lastTime = 0;
+let gameOver = false;
+let ravens = [];
+class Raven {
+	constructor(){
+		this.spriteWidth = 271;
+		this.spriteHeight = 194;
+		this.sizeModifier = Math.random() * 0.6 + 0.4;
+		this.width = this.spriteWidth * this.sizeModifier;
+		this.height = this.spriteHeight * this.sizeModifier;
+		this.x = canvas.width;
+		this.y = Math.random() * (canvas.height - this.height);
+		this.directionX = Math.random() * 5 + 3; // change to *5 + 1 to bounce
+		this.directionY = Math.random() * 5 - 2.5; // change 2.5 to get bounce effecct
+		this.markedForDeletion = false;
+		this.image = new Image();
+		this.image.src = 'raven.png';
+		this.frame = 0;
+		this.maxFrame = 4;
+		this.timeSinceFlap = 0;
+		this.flapInterval = Math.random() * 50 + 50; // 25 + 25 to bounce
+		
+	}
+	update(deltatime){
+		if (this.y < 0 || this.y > canvas.height - this.height){
+			this.directionY = this.directionY * -1;
+		}
+		this.x -= this.directionX;
+		this.y += this.directionY;
+		if (this.x < 0 - this.width) this.markedForDeletion = true;
+		this.timeSinceFlap += deltatime;
+		if (this.timeSinceFlap > this.flapInterval){
+			if (this.frame > this.maxFrame) this.frame = 0;
+		else this.frame++;
+		this.timeSinceFlap = 0;
+		}
+	}
+	draw(){
+		ctx.strokeRect(this.x, this.y, this.width, this.height);
+		ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+	}
+}
+
+function drawScore(){
+	ctx.fillStyle = 'black';
+	ctx.fillText('Score: ' + score, 50, 75); 
+	ctx.fillStyle = 'white';
+	ctx.fillText('Score: ' + score, 55, 80);// coordinates
+}
+
+window.addEventListener('click', function(e){
+	const detectPixelColor = ctx.getImageData(e.x, e.y, 1, 1); // x and y within height of area we want to scan - just one pixel
+	console.log(detectPixelColor)
+});
+
+
+
+// timestamp is default JS, will callback animate. animate gets called over and over
+function animate(timestamp){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	let deltatime = timestamp - lastTime; //delta time is difference in milliseconds between last frame and current frame
+	lastTime = timestamp;
+	timeToNextRaven += deltatime; //approx 16ms per frame on my pc
+	if (timeToNextRaven > ravenInterval){
+		ravens.push(new Raven());
+		timeToNextRaven = 0;
+	};
+
+
+	drawScore(); // called behind ravens 
+	[...ravens].forEach(object => object.update(deltatime)); 
+	[...ravens].forEach(object => object.draw()); 
+	ravens = ravens.filter(object => !object.markedForDeletion);
+	requestAnimationFrame(animate);
+}
+animate(0);
